5e97e65c61bac2e37b11d4ffaf854760d1541112
jamesdiplomat <42521639+jamesdiplomat@users.noreply.github.com>
2020-02-14 18:57:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 
 path = Path("path")
 classes = ['cleanwater', 'dirtywater']
-data2 = ImageDataBunch.single_from_classes(path, classes, tfms=get_transforms(), size=224).normalize(imagenet_stats)
+data2 = ImageDataBunch.single_from_classes(path, classes, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
 learn = create_cnn(data2, models.resnet34)
 learn.load('stage-2')
 
